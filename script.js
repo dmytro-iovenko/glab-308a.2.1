b2ee20f58b01dba8ddaf47f9d900d8b4df7db922
Dmytro Iovenko <iovenko@gmail.com>
2024-08-22 21:58:49 -0400

    add loop to log each item in inventory

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("OK")
+const adventurer = {
+  name: "Robin",
+  health: 10,
+  inventory: ["sword", "potion", "artifact"],
+};
+
+
+adventurer.inventory.forEach(item => {
+    console.log(`${adventurer.name} has ${item} in his inventory.`)
+});
