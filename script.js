844b3443a5ac218f6cb07498102ca8fb587a8cdf
Dmytro Iovenko <iovenko@gmail.com>
2024-08-22 22:01:39 -0400

    add roll method

--- script.js
+++ script.js
@@ -11,8 +11,17 @@ const adventurer = {
       inventory: ["small hat", "sunglasses"],
     },
   },
+  roll(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`);
+  },
 };
 
 adventurer.inventory.forEach((item) => {
   console.log(`${adventurer.name} has ${item} in his inventory.`);
 });
+
+// Test roll method by calling it a few times
+adventurer.roll()
+adventurer.roll()
+adventurer.roll()
