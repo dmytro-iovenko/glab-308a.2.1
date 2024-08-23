fe66ba4dfb8c823a052edbca170ac9e5d57e60ef
Dmytro Iovenko <iovenko@gmail.com>
2024-08-22 22:00:28 -0400

    add companions

--- script.js
+++ script.js
@@ -2,9 +2,17 @@ const adventurer = {
   name: "Robin",
   health: 10,
   inventory: ["sword", "potion", "artifact"],
+  companion: {
+    name: "Leo",
+    type: "Cat",
+    companion: {
+      name: "Frank",
+      type: "Flea",
+      inventory: ["small hat", "sunglasses"],
+    },
+  },
 };
 
-
-adventurer.inventory.forEach(item => {
-    console.log(`${adventurer.name} has ${item} in his inventory.`)
+adventurer.inventory.forEach((item) => {
+  console.log(`${adventurer.name} has ${item} in his inventory.`);
 });
