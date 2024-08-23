e8d4760b20694ad4c2ab7dca3697a93855274ed4
Dmytro Iovenko <iovenko@gmail.com>
2024-08-22 22:13:31 -0400

    change the declaration of Robin and the companions to use the new Adventurer and Companion classes

--- script.js
+++ script.js
@@ -66,12 +66,10 @@ class Companion extends Character {
 }
 
 // re-create Robin using the Character class
-const robin = new Character("Robin");
+const robin = new Adventurer("Robin");
 robin.inventory = ["sword", "potion", "artifact"];
-robin.companion = new Character("Leo");
-robin.companion.type = "Cat";
-robin.companion.companion = new Character("Frank");
-robin.companion.companion.type = "Flea";
+robin.companion = new Companion("Leo", "Cat");
+robin.companion.companion = new Character("Frank", "Flea");
 robin.companion.companion.inventory = ["small hat", "sunglasses"];
 
 robin.inventory.forEach((item) => {
